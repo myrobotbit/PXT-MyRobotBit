4494269d58fa0b95ee19c13e7a49be8abde636e6
myrobotbit <41653060+myrobotbit@users.noreply.github.com>
2018-10-23 19:32:27 +0700

    Update MyRobotBit.ts

--- MyRobotBit.ts
+++ MyRobotBit.ts
@@ -34,7 +34,7 @@ enum Turn {
 /**
  * Custom blocks
  */
-//% weight=100 color=#ff00cc weight=10 icon="\uf11e"
+//% weight=100 color=#ff9900 weight=10 icon="\uf11e"
 namespace MyRobotBit {
 
     /**MotorON          Control motor channel direction and speed.   
